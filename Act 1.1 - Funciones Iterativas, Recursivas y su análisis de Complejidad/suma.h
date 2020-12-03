9e0f3d7ed49e84e87d703558e3edf4f292e6a5de
JesusUgaldeResendiz <64572948+JesusUgaldeResendiz@users.noreply.github.com>
2020-12-03 12:06:22 -0600

    Update suma.h

--- Act 1.1 - Funciones Iterativas, Recursivas y su análisis de Complejidad/suma.h
+++ Act 1.1 - Funciones Iterativas, Recursivas y su análisis de Complejidad/suma.h
@@ -3,41 +3,43 @@
 
 using namespace std;
 
-int sumaIterativa(int n)
-{
-	int acum = 0;
-	if(n<1){
-		return 0;
-	}
-	else{
-		for(int i =0;i<=n;i++){
-			acum+=i;
+class Funciones{
+	public:
+		Funciones(){}
+		int sumaRecursiva(int n){
+			if(n<1){
+				return 0;
+			}
+			else{
+				return n+sumaRecursiva(n-1);
+			}
 		}
-		return acum;
-	}
-}
+		
+		int sumaIterativa(int n){
+			int acum = 0;
+			if(n<1){
+				return 0;
+			}
+			else{
+				for(int i=0; i<=n; i++){
+					acum += i;
+				}
+				return acum;
+			}
+		}
+		
+		int sumaDirecta(int n){
+			if(n<1){
+				return 0;
+			}
+			else{
+				int result;
+				result = (n*(n+1))/2;
+				return result;
+			}
+		}
+		
+};
 
-int sumaRecursiva(int n)
-{
-	if(n<1){
-		return 0;
-	}
-	else{
-		return n+sumaRecursiva(n-1);
-	}
-}
-
-int sumaDirecta(int n)
-{
-  if(n<1){
-    return 0;
-  }
-  else{
-    int result;
-    result = (n*(n+1))/2;
-    return result;
-  }
-
-}
 
 #endif // SUMA_H_INCLUDED
